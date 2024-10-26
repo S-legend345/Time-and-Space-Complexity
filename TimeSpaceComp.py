e9d679ab92a2971a8814d07a61bd6bf4064f4165
Sam Mathew <76838983+S-legend345@users.noreply.github.com>
2024-10-26 18:23:49 +0530

    Add files via upload

--- TimeSpaceComp.py
+++ TimeSpaceComp.py
@@ -1,44 +1,63 @@
-def summation(n):
-    sum = n*(n+1)/2
-    print(sum)
+# def summation(n):
+#     sum = n*(n+1)/2
+#     print(sum)
 
-summation(1)
-summation(2)
-summation(3)
-summation(4)
-summation(5)
-
-
-def add(n):
-    summ = 0
-    for i in range(1,n+1):
-        summ += i
-    print(summ)
-
-add(15)
-
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num%i == 0:
-            return False
-    return True
-
-def first_prime_num(n):
-    primes = []
-    num = 2
-    while len(primes) < 10:
-        if is_prime(num):
-            primes.append(num)
-        num += 1
-    return primes
-
-print(first_prime_num(12))
+# summation(1)
+# summation(2)
+# summation(3)
+# summation(4)
+# summation(5)
 
 
-def squares(a):
-    for i in range(1, a+1):
-        print(i**2)
+# def add(n):
+#     summ = 0
+#     for i in range(1,n+1):
+#         summ += i
+#     print(summ)
 
-squares(10)
+# add(15)
+
+# def is_prime(num):
+#     if num < 2:
+#         return False
+#     for i in range(2, int(num**0.5) + 1):
+#         if num%i == 0:
+#             return False
+#     return True
+
+# def first_prime_num(n):
+#     primes = []
+#     num = 2
+#     while len(primes) < 10:
+#         if is_prime(num):
+#             primes.append(num)
+#         num += 1
+#     return primes
+
+# print(first_prime_num(12))
+
+
+# def squares(a):
+#     for i in range(1, a+1):
+#         print(i**2)
+
+# squares(10)
+
+#ACP - Time Space Complexity
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+def multiply1(n1, n2):
+    return n1 * n2
+
+
+def multiplyn(n1, n2):
+    
+    count = 0
+    for i in range(n2):
+        count += n1
+    return count
+
+print(multiply1(n1,n2))
+print(multiplyn(n1,n2))
